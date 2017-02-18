662727a6dde8e2d6b0b4f286d9c54b4366dcb431
Ashish <ashish.khatiwada3@gmail.com>
2017-02-19 00:50:03 +0545

    Update Dbconn.java

--- src/main/java/com/mycompany/furniture/Dbutil/Dbconn.java
+++ src/main/java/com/mycompany/furniture/Dbutil/Dbconn.java
@@ -9,8 +9,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 
@@ -26,16 +24,14 @@ public class Dbconn {
     public static Connection getConnection(){
           
         try {
-            Class.forName("org.postgresql.Driver");
-            conn =DriverManager.getConnection("postgres://ttbnfcvkbdmugw:dca0dd23f1eba76b36362e66c3c7cdb11e601230e4dc33f5462e16e6f21814ef@ec2-54-225-66-44.compute-1.amazonaws.com:5432/d87uveini62cv5","ttbnfcvkbdmugw","dca0dd23f1eba76b36362e66c3c7cdb11e601230e4dc33f5462e16e6f21814ef");
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            conn =DriverManager.getConnection(dbUrl);
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Dbconn.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn;
     }
-     public void createtables(){
+  public void createtables(){
            String sql = "CREATE TABLE tbl_user_signup(ID integer PRIMARY KEY,Email varchar(255) NOT NULL,Password varchar(50) NOT NULL)";
 
         Statement ps;
@@ -43,7 +39,8 @@ public class Dbconn {
             ps = getConnection().createStatement();
             ps.executeUpdate(sql);
         } catch (SQLException ex) {
-            Logger.getLogger(Dbconn.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
     }
 }
+

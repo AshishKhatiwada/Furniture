7c5e46b4095b55bcb93b6720300e5bc090de37ba
Ashish <ashish.khatiwada3@gmail.com>
2017-02-19 00:05:07 +0545

    Update Dbconn.java

--- src/main/java/com/mycompany/furniture/Dbutil/Dbconn.java
+++ src/main/java/com/mycompany/furniture/Dbutil/Dbconn.java
@@ -7,7 +7,6 @@ package com.mycompany.furniture.Dbutil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -27,10 +26,12 @@ public class Dbconn {
     public static Connection getConnection(){
           
         try {
-            
+            Class.forName("org.postgresql.Driver");
             conn =DriverManager.getConnection("postgres://ttbnfcvkbdmugw:dca0dd23f1eba76b36362e66c3c7cdb11e601230e4dc33f5462e16e6f21814ef@ec2-54-225-66-44.compute-1.amazonaws.com:5432/d87uveini62cv5","ttbnfcvkbdmugw","dca0dd23f1eba76b36362e66c3c7cdb11e601230e4dc33f5462e16e6f21814ef");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Dbconn.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn;
     }
